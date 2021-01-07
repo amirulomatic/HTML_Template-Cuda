22ff08acd127b4877f1071fa175faa8e2e2cfc6a
amirul <amirulhridoy1@gmail.com>
2021-01-07 18:51:47 +0600

    Menu edited

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,21 @@ $(document).ready(function(){
 
 
     $('.hamburger-menu a').click(function(){
+        
+
+        let menuStatus = $('.menu').css('display');
+        
+        if(menuStatus == 'block'){
+            $('.hamburger-menu a').html('<i class="fas fa-bars"></i>');
+            
+        }else if(menuStatus == 'none'){
+            $('.hamburger-menu a').html('<i class="fas fa-times"></i>');
+        };
+
+
         $('.menu').slideToggle();
-    })
+    });
+
 
 
 
